76425d1d799641b2919085d3c827b51aaad97e37
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2023-01-08 13:06:18 -0800

    add network and image assets and elevated button for login

--- lib/main.dart
+++ lib/main.dart
@@ -26,27 +26,41 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tutorial Blog'),
-        centerTitle: true,
-      ),
+      backgroundColor: Colors.red,
       body: Center(
         child: Column(
-          children: const [
-            Text(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
               'Welcome',
               style: TextStyle(
-                color: Colors.red,
+                color: Colors.white,
                 fontSize: 55,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            Text('Welcome'),
-            Text('Welcome'),
-            SizedBox(height: 20.0, width: 20.0),
-            Text('Welcome'),
-            Text('Welcome'),
-            Text('Welcome'),
+            const Text(
+              'To Blog',
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            // Image.network(
+            //     'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/89fbb625-5ea0-4464-b411-969601c986aa/df4bhd7-b29944f7-b79a-4092-a081-869bc31c54d8.png/v1/fill/w_1024,h_1280,q_80,strp/clio__gnome_bard_commission_by_sara_scmp_df4bhd7-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MCIsInBhdGgiOiJcL2ZcLzg5ZmJiNjI1LTVlYTAtNDQ2NC1iNDExLTk2OTYwMWM5ODZhYVwvZGY0YmhkNy1iMjk5NDRmNy1iNzlhLTQwOTItYTA4MS04NjliYzMxYzU0ZDgucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.48R6PxU8psvurxwR4AUIc04_sSfCTCLSWjLtbxgZXAQ'),
+            Image.asset('images/gnomeBard.jpg'),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.red,
+                minimumSize: const Size(150, 40),
+              ),
+              child: const Text('Login'),
+            )
           ],
         ),
       ),
